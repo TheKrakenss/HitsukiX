86ad53fc33d9565ab3437c6efcb494c162a88037
TheKrakenss <87159731+TheKrakenss@users.noreply.github.com>
2021-11-24 09:38:43 +0700

    Change name to Ayn on module misc

--- hitsuki/modules/misc.py
+++ hitsuki/modules/misc.py
@@ -259,7 +259,7 @@ A module with some useful commands but without a specific category.
 <b>Available commands:</b>
 - /direct (url): Generates direct links from the sourceforge.net
 - /github (username): Returns info about a GitHub user or organization.
-- /cancel: Disables current state. Can help in cases if Hitsuki not responing on your message.
+- /cancel: Disables current state. Can help in cases if Ayn not responing on your message.
 - /id: get the current group id. If used by replying to a message, gets that user's id.
 - /info: get information about a user.
 - /afk (reason): Mark yourself as AFK. When marked as AFK, any mentions will be replied to with a message stating that you're not available!
